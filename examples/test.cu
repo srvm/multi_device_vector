21ea82b71b93f853788a74bd4ea981e4a1b5fdfb
Saurav Muralidharan <sauravm@nvidia.com>
2014-07-15 16:56:51 -0700

    Initial commit

---
+++ examples/test.cu
@@ -0,0 +1,28 @@
+#include <ctime>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include <multi_device_vector.h>
+
+static constexpr int N = 901;
+
+int main() {
+  using value_t = float;
+
+  thrust::host_vector<value_t> h_vector(N, 0);
+
+  std::srand(time(NULL));
+  thrust::generate(h_vector.begin(), h_vector.end(),
+      []() { return value_t(std::rand()) / value_t(RAND_MAX); });
+
+  thrust::multi_device_vector<value_t> v1(h_vector);
+  thrust::multi_device_vector<value_t> v2(N, 0);
+
+  // Copy everything in v1 to v2
+  //thrust::copy(v1.begin(), v1.end(), v2.begin());
+
+  //thrust::host_vector<value_t> result(v2.begin(), v2.end());
+
+  return 0;
+}
---
+++ include/multi_device_vector.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <thrust/device_vector.h>
+#include "util.h"
+
+namespace thrust {
+
+  template<typename base_iterator>
+  struct multi_device_iterator {
+  };
+
+  template<typename T>
+  struct multi_device_vector {
+    typedef multi_device_vector<T> self_t;
+    typedef thrust::device_vector<T> base_vector_t;
+    typedef std::vector<base_vector_t *> vector_t;
+    typedef typename base_vector_t::iterator base_iterator;
+    typedef multi_device_iterator<base_iterator> iterator;
+
+    multi_device_vector() {}
+
+    explicit multi_device_vector(int size, const T& prefix) {
+      cudaGetDeviceCount(&m_tiles);
+      m_tile_size = util::__ceil(size, m_tiles);
+
+      int allocated = 0;
+      for(int i = 0; i < m_tiles - 1; ++i) {
+        base_vector_t *p = new base_vector_t(m_tile_size, prefix);
+        m_data.push_back(p);
+        allocated += m_tile_size;
+      }
+
+      // Allocate final chunk
+      base_vector_t *p = new base_vector_t(size - allocated, prefix);
+      m_data.push_back(p);
+    }
+
+    template<typename _iterator>
+    multi_device_vector(_iterator begin, _iterator end) {
+      cudaGetDeviceCount(&m_tiles);
+      const int size = end-begin;
+      m_tile_size = util::__ceil(size, m_tiles);
+
+      int allocated = 0;
+      for(int i = 0; i < m_tiles - 1; ++i) {
+        base_vector_t *p = new base_vector_t(begin + allocated,
+                           begin + allocated + m_tile_size);
+        m_data.push_back(p);
+        allocated += m_tile_size;
+      }
+
+      // Allocate final chunk
+      base_vector_t *p = new base_vector_t(begin + allocated, begin + size);
+      m_data.push_back(p);
+    }
+
+    template<typename S>
+    multi_device_vector(const S& s) : self_t(s.begin(), s.end()) {}
+
+    ~multi_device_vector() {
+      for(int i = 0; i < m_tiles; ++i)
+        delete m_data[i];
+    }
+
+  private:
+    int m_tiles;
+    int m_tile_size;
+    vector_t m_data;
+  };
+
+} // namespace thrust
