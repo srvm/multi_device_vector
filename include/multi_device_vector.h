ad25fcb0ea996298a7f5903c9eff09e3cb320d0e
Saurav Muralidharan <sauravm@nvidia.com>
2014-07-15 17:24:30 -0700

    Adding setdevice

--- include/multi_device_vector.h
+++ include/multi_device_vector.h
@@ -14,6 +14,7 @@ namespace thrust {
     typedef multi_device_vector<T> self_t;
     typedef thrust::device_vector<T> base_vector_t;
     typedef std::vector<base_vector_t *> vector_t;
+    //typedef thrust::device_vector<pointer_type> vector_storage_t;
     typedef typename base_vector_t::iterator base_iterator;
     typedef multi_device_iterator<base_iterator> iterator;
 
@@ -25,11 +26,13 @@ namespace thrust {
 
       int allocated = 0;
       for(int i = 0; i < m_tiles - 1; ++i) {
+        cudaSetDevice(i);
         base_vector_t *p = new base_vector_t(m_tile_size, prefix);
         m_data.push_back(p);
         allocated += m_tile_size;
       }
 
+      cudaSetDevice(m_tiles - 1);
       // Allocate final chunk
       base_vector_t *p = new base_vector_t(size - allocated, prefix);
       m_data.push_back(p);
@@ -43,12 +46,14 @@ namespace thrust {
 
       int allocated = 0;
       for(int i = 0; i < m_tiles - 1; ++i) {
+        cudaSetDevice(i);
         base_vector_t *p = new base_vector_t(begin + allocated,
                            begin + allocated + m_tile_size);
         m_data.push_back(p);
         allocated += m_tile_size;
       }
 
+      cudaSetDevice(m_tiles - 1);
       // Allocate final chunk
       base_vector_t *p = new base_vector_t(begin + allocated, begin + size);
       m_data.push_back(p);
@@ -66,6 +71,7 @@ namespace thrust {
     int m_tiles;
     int m_tile_size;
     vector_t m_data;
+    //vector_storage_t m_pointers;
   };
 
 } // namespace thrust
